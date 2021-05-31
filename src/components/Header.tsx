97b42c9aba374220178457708a67057bf0668ccf
Valerii22 <valera.vakalov22@gmail.com>
2021-05-31 12:00:40 +0200

    Created Header, Navbar and Profile components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header_logo from '../images/header_logo/Header_logo.png';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src={Header_logo} alt=""/>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://www.noroctech.com/wp-content/uploads/2020/10/webdes2.jpg"/>
+            </div>
+            <div>
+                ava+ description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile
